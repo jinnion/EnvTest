14680a95c528d73a3269ebba9553eb3afa2428e0
livetory <jinnion@gmail.com>
2020-06-18 20:59:50 +0900

    sentry Test

--- app/src/main/java/com/example/envtest/MainActivity.java
+++ app/src/main/java/com/example/envtest/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.sentry.core.Sentry;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            throw new Exception("This is  a test.");
+        } catch(Exception e) {
+            Sentry.captureException(e);
+        }
+        // setContentView(R.layout.activity_main);
         // Temporary Text
     }
 }
